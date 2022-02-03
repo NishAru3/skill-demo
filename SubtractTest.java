795d38c40955e951141ac3b8192a6986f461fead
Nishanth Arumugam <nishantharu3@gmail.com>
2022-02-02 20:33:19 -0800

    Fixed the test

--- SubtractTest.java
+++ SubtractTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SubtractTest {
     @Test
     public void testSubtract(){
-        assertEquals(4, Subtract.subtractResult(10,2));
+        assertEquals(4, Subtract.subtractResult(10,6));
     }
 }
